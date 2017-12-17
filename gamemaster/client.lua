3f61ab5ac3b50f683d907e34f802b17b26e96049
memoryleak47 <memoryleak47@protonmail.ch>
2017-12-17 22:05:02 +0100

    now the client handles packets

--- gamemaster/client.lua
+++ gamemaster/client.lua
@@ -1,3 +1,5 @@
+local packetizer_mod = require("packetizer")
+
 function new_clientmaster(networker, player_count, local_id)
 	local clientmaster = require("game/mod").new(player_count, local_id)
 
@@ -6,6 +8,11 @@ function new_clientmaster(networker, player_count, local_id)
 
 	function clientmaster:send(p)
 		clientmaster.networker:send_to_server(p)
+	end
+
+	function clientmaster:on_recv(p)
+		local changed_inputs, player_id, frame_id = packetizer_mod.packet_to_inputs(p)
+		clientmaster.calendar:apply_input_changes(changed_inputs, player_id, frame_id)
 	end
 
 	print("client - gamemaster alive!")
