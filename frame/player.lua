24e1b33689c15c67ee49592a206c20cd4ee1ebd6
memoryleak47 <memoryleak47@protonmail.ch>
2017-12-27 14:07:36 +0100

    defined MOVESPEED

--- frame/player.lua
+++ frame/player.lua
@@ -4,6 +4,8 @@ local vec_mod = require('space/vec')
 function new_player(char)
 	local player = {}
 
+	local MOVESPEED = 0.5 -- in world coordinates per tick()
+
 	player.shape = rect_mod.by_center_and_size(
 		vec_mod(0, 0),
 		vec_mod(10, 10)
@@ -23,11 +25,11 @@ function new_player(char)
 
 		if self.walk_target ~= nil then
 			local move_vec = self.walk_target - self.shape:center()
-			if move_vec:length() < 1 then
+			if move_vec:length() < MOVESPEED then
 				self.shape = self.shape:with_center_keep_size(self.walk_target)
 				self.walk_target = nil
 			else
-				self.shape.center_vec = self.shape:center() + move_vec:normalized()
+				self.shape.center_vec = self.shape:center() + move_vec:with_length(MOVESPEED)
 			end
 		end
 
