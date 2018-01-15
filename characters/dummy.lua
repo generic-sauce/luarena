130f70da5e1459075e5553ff3f04bc9b69443f58
memoryleak47 <memoryleak47@protonmail.ch>
2018-01-15 20:40:13 +0100

    ...

--- characters/dummy.lua
+++ characters/dummy.lua
@@ -1,9 +1,17 @@
 local rect_mod = require('space/rect')
 local vec_mod = require('space/vec')
 
+local REGEN_INTERVAL = 5
+
 return function (dummy)
+	dummy.health_counter = 0
+
 	function dummy:char_tick(frame)
-		self.health = math.min(self.health + 1, 100)
+		self.health_counter = (self.health_counter + 1) % REGEN_INTERVAL
+
+		if self.health_counter == 0 then
+			self.health = math.min(self.health + 1, 100)
+		end
 	end
 
 	return dummy
