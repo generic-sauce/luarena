3656337c7d2674f2764ba1118a89151bbc972407
memoryleak47 <memoryleak47@protonmail.ch>
2018-01-18 01:58:19 +0100

    implemented polygon-circle collision detection

--- frame/collision/detection.lua
+++ frame/collision/detection.lua
@@ -1,5 +1,46 @@
 local function colliding_polygon_circle(p, c)
-	assert(false, "TODO")
+	local function vec3d(u)
+		return { u[1], u[2], 1 }
+	end
+
+	local function cross(u, v)
+		return {
+			u[2] * v[3] - u[3] * v[2],
+			u[3] * v[1] - u[1] * v[3],
+			u[1] * v[2] - u[2] * v[1]
+		}
+	end
+
+	local function dot(u, v)
+		return u[1] * v[1] + u[2] * v[2] + u[3] * v[3]
+	end
+
+	-- checks whether the circle c is completely on the right side of the line `line_start to line_end`
+	local function is_on_right_side(line_start, line_end, c)
+		local line_right_vec3d = cross(vec3d(line_start), vec3d(line_end))
+		local line_right_vec = vec_mod(line_right_vec3d[1], line_right_vec3d[2])
+
+		local move_vec = line_right_vec:with_length(c.radius)
+
+		-- move imaginary line to the right, as much as the radius of c
+		local moved_line_start = line_start + move_vec
+		local moved_line_end = line_end + move_vec
+
+		local vec_from_moved_line_start_to_center = vec3d(c:center() - moved_line_start)
+		return dot(vec_from_moved_line_start_to_center, line_right_vec3d) >= 0
+	end
+
+	local function has_separating_axis_to(points, c)
+		for i, u in pairs(points) do
+			local v = points[i + 1] or points[1]
+			if is_on_right_side(u, v, c) then
+				return true
+			end
+		end
+		return false
+	end
+
+	return not has_separating_axis_to(p:abs_points(), c)
 end
 
 local function colliding_circles(c1, c2)
