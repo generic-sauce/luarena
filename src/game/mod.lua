b3fcda8d650d66d92d1e738fa1a3c635a87ded55
memoryleak47 <memoryleak47@protonmail.ch>
2018-02-17 23:40:42 +0100

    made the frame_history a ring-buffer

--- src/game/mod.lua
+++ src/game/mod.lua
@@ -1,6 +1,7 @@
 FRAME_DURATION = 0.005 -- in seconds
 INPUT_DELAY = 10
 BACKTRACK_BALANCE_INTERVAL = 2000
+FRAME_HISTORY_LENGTH = math.ceil(5 / FRAME_DURATION)
 
 local game_mod = {}
 local frame_mod = require("frame/mod")
@@ -11,6 +12,8 @@ local dev = require('dev')
 
 require("misc")
 
+local NO_FRAME = {}
+
 function game_mod.new(chars, local_id)
 	local game = {}
 	game.current_frame = frame_mod.initial(chars)
@@ -18,33 +21,41 @@ function game_mod.new(chars, local_id)
 			cam_mod.following(local_id)
 			-- cam_mod.fixed(game.current_frame.map:rect():center())
 	game.frame_history = {}
+	game.frame_counter = 0 -- in the first frames the same as #frame_history
 	game.chars = chars
 	game.calendar = calendar_mod.new(#chars, local_id)
 	game.local_id = local_id
 	game.start_time = love.timer.getTime()
+
+	for i=1, FRAME_HISTORY_LENGTH do
+		game.frame_history[i] = NO_FRAME
+	end
 
 	-- frame_id is the oldest frame to be re-calculated
 	function game:backtrack(frame_id)
 		dev.start_profiler("backtrack", {"backtrack"})
 
 		local c = 0
-		local fh_size = #self.frame_history
-		while frame_id <= #self.frame_history do
+		local fh_size = self.frame_counter
+		while frame_id <= self.frame_counter do
 			c = c + 1
-			self.frame_history[#self.frame_history] = nil
+			set(self.frame_history, self.frame_counter, NO_FRAME)
+			self.frame_counter = self.frame_counter - 1
 		end
 
 		if frame_id == 1 then
 			self.current_frame = frame_mod.initial(game.chars)
 		else
-			self.current_frame = self.frame_history[#self.frame_history]:clone()
+			local old_frame = get(self.frame_history, self.frame_counter)
+			assert(old_frame ~= NO_FRAME, "backtracking too far!")
+			self.current_frame = old_frame:clone()
 		end
 
 		local current_time = love.timer.getTime()
-		while #self.frame_history * FRAME_DURATION < current_time - self.start_time do
+		while self.frame_counter * FRAME_DURATION < current_time - self.start_time do
 			self:frame_update()
 		end
-		dev.debug("went back " .. c .. " frames (" .. fh_size .. " -> " .. frame_id-1 .. " -> " .. #self.frame_history .. ")", {"backtrack"})
+		dev.debug("went back " .. c .. " frames (" .. fh_size .. " -> " .. frame_id-1 .. " -> " .. self.frame_counter .. ")", {"backtrack"})
 
 		dev.stop_profiler("backtrack")
 	end
@@ -59,7 +70,7 @@ function game_mod.new(chars, local_id)
 			end
 		end
 
-		if oldest_frame_id <= #self.frame_history then
+		if oldest_frame_id <= self.frame_counter then
 			self:backtrack(oldest_frame_id)
 		end
 	end
@@ -70,22 +81,23 @@ function game_mod.new(chars, local_id)
 
 		if next(changed_inputs) == nil then return end
 
-		self.calendar:apply_input_changes(changed_inputs, self.local_id, #self.frame_history + 1 + INPUT_DELAY)
+		self.calendar:apply_input_changes(changed_inputs, self.local_id, self.frame_counter + 1 + INPUT_DELAY)
 
 		self:send({
 			tag = "inputs",
 			inputs = changed_inputs,
 			player_id = self.local_id,
-			frame_id = #self.frame_history + 1 + INPUT_DELAY
+			frame_id = self.frame_counter + 1 + INPUT_DELAY
 		})
 	end
 
 	function game:frame_update()
 		dev.start_profiler("frame_update")
 
-		self.calendar:apply_to_frame(self.current_frame, #self.frame_history + 1)
+		self.calendar:apply_to_frame(self.current_frame, self.frame_counter + 1)
 		self.current_frame:tick()
-		table.insert(self.frame_history, self.current_frame:clone())
+		set(self.frame_history, self.frame_counter, self.current_frame:clone())
+		self.frame_counter = self.frame_counter + 1
 
 		dev.stop_profiler("frame_update")
 	end
@@ -96,7 +108,7 @@ function game_mod.new(chars, local_id)
 		self:update_local_calendar()
 
 		local current_time = love.timer.getTime()
-		while #self.frame_history * FRAME_DURATION < current_time - self.start_time do
+		while self.frame_counter * FRAME_DURATION < current_time - self.start_time do
 			if self.gamemaster_update then
 				self:gamemaster_update()
 			end
