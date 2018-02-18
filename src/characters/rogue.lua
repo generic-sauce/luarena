a4ca2d82899816f177afa60242ea7288313d3e1a
memoryleak47 <memoryleak47@protonmail.ch>
2018-02-19 00:13:26 +0100

    "balanced" and fixed rogue

--- src/characters/rogue.lua
+++ src/characters/rogue.lua
@@ -2,16 +2,23 @@ local rect_mod = require('viewmath/rect')
 local vec_mod = require('viewmath/vec')
 local circle_mod = require('shape/circle')
 
+local S1_COOLDOWN = 300
+local S1_RANGE = 200
+
+local S2_COOLDOWN = 300
+local S2_DAMAGE = 25
+local S2_RADIUS = 40
+
 return function (rogue)
 
-	rogue.h_cooldown = 0
-	rogue.j_cooldown = 0
+	rogue.s1_cooldown = 0
+	rogue.s2_cooldown = 0
 
 	function rogue:new_aoe(frame)
 		local aoe = {}
 
 		aoe.owner = self
-		aoe.shape = circle_mod.by_center_and_radius(self.shape:center(), 100)
+		aoe.shape = circle_mod.by_center_and_radius(self.shape:center(), S2_RADIUS)
 		aoe.life_counter = 100
 
 		function aoe:initial_damage(frame)
@@ -19,7 +26,7 @@ return function (rogue)
 				if entity ~= self
 					and entity ~= self.owner
 					and entity.damage then
-						entity:damage(20)
+						entity:damage(S2_DAMAGE)
 				end
 			end
 		end
@@ -42,21 +49,19 @@ return function (rogue)
 
 
 	function rogue:char_tick(frame)
-		self.h_cooldown = math.max(0, self.h_cooldown - 1)
-		self.j_cooldown = math.max(0, self.j_cooldown - 1)
+		self.s1_cooldown = math.max(0, self.s1_cooldown - 1)
+		self.s2_cooldown = math.max(0, self.s2_cooldown - 1)
 
-		if self.inputs.h and self.h_cooldown == 0 then
-			local MAX_JUMP = 200
+		if self.inputs[S1_KEY] and self.s1_cooldown == 0 then
+			self.s1_cooldown = S1_COOLDOWN
 
-			self.h_cooldown = 100
-
-			local jump = self:direction():with_length(MAX_JUMP)
+			local jump = self:direction():with_length(S1_RANGE)
 
 			self.shape = self.shape:move_center(jump)
 		end
 
-		if self.inputs.j and self.j_cooldown == 0 then
-			self.j_cooldown = 100
+		if self.inputs[S2_KEY] and self.s2_cooldown == 0 then
+			self.s2_cooldown = S2_COOLDOWN
 			frame:add(self:new_aoe(frame))
 		end
 	end
