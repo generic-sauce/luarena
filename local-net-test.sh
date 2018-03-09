7e1c4fcc43aece676b3f36a131207f52bcb363c0
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-09 14:27:19 +0100

    ...

--- local-net-test.sh
+++ local-net-test.sh
@@ -5,6 +5,7 @@ DELAYMAN_PORT="3840"
 
 ./delayman.py $SERVER_PORT $DELAYMAN_PORT &
 ./run.sh server u1 $SERVER_PORT &
+sleep 0.1 # this is required, so that the server is always the first!
 ./run.sh client u1 "127.0.0.1" $DELAYMAN_PORT
 
 pkill delayman.py
