f865e864b89933f61177f149ef271bd5e8fda4bf
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-17 14:27:35 +0100

    gained major performance improvements by refactoring the vec-module

--- src/viewmath/vec.lua
+++ src/viewmath/vec.lua
@@ -1,94 +1,87 @@
 -- vectors are immutable atomic data structures, never access their members directly
 
-function vec_mod(x, y)
-	assert(x)
-	assert(y)
-
-	local meta = {}
-	function meta:__index(i)
+local meta = {}
+meta.__index = {
+	index = function (i)
 		if i == 1 then
 			return self.x
 		elseif i == 2 then
 			return self.y
 		else
-			assert(false, "Vec: Index '" .. i .. "' out of range")
+			assert(false, "vec:index - index out of range")
 		end
-	end
+	end,
 
-	function meta:__add(other)
-		return vec_mod(self.x + other.x, self.y + other.y)
-	end
-
-	function meta:__sub(other)
-		return vec_mod(self.x - other.x, self.y - other.y)
-	end
-
-	function meta:__mul(other)
-		if type(other) ~= "table" then
-			return vec_mod(self.x * other, self.y * other)
-		end
-		return vec_mod(self.x * other.x, self.y * other.y)
-	end
-
-	function meta:__div(other)
-		if type(other) ~= "table" then
-			return vec_mod(self.x / other, self.y / other)
-		end
-		return vec_mod(self.x / other.x, self.y / other.y)
-	end
-
-	function meta:__tostring()
-		return "vec(" .. tostring(self.x) .. ", " .. tostring(self.y) .. ")"
-	end
-
-	local v = setmetatable({ x = x, y = y }, meta)
-
-	function v:length()
+	length = function(self)
 		return math.sqrt(self.x^2 + self.y^2)
-	end
-
-	function v:with_x(x)
+	end,
+	with_x = function(self, x)
 		return vec_mod(x, self.y)
-	end
-
-	function v:with_y(y)
+	end,
+	with_y = function(self, y)
 		return vec_mod(self.x, y)
-	end
-
-	function v:floor()
+	end,
+	floor = function(self)
 		return vec_mod(math.floor(self.x), math.floor(self.y))
-	end
-
-	function v:ceil()
+	end,
+	ceil = function(self)
 		return vec_mod(math.ceil(self.x), math.ceil(self.y))
-	end
-
-	function v:normalized()
+	end,
+	normalized = function(self)
 		return self / self:length()
-	end
-
-	function v:cropped_to(l)
+	end,
+	cropped_to = function(self, l)
 		if self:length() > l then
 			return self:normalized() * l
 		else
 			return self
 		end
-	end
-
-	function v:with_length(l)
+	end,
+	with_length = function(self, l)
 		return self:normalized() * l
-	end
-
-	function v:dot(w)
-		return self[1] * w[1] + self[2] * w[2]
-	end
-
-	function v:cross(w)
+	end,
+	dot = function(self, w)
+		return self.x * w.x + self.y * w.y
+	end,
+	cross = function(self, w)
 		return vec_mod(
-			self[2] * 1 - 1 * w[2],
-			1 * w[1] - self[1] * 1
+			self.y * 1 - 1 * w.y,
+			1 * w.x - self.x * 1
 		)
 	end
+}
+
+function meta:__add(other)
+	return vec_mod(self.x + other.x, self.y + other.y)
+end
+
+function meta:__sub(other)
+	return vec_mod(self.x - other.x, self.y - other.y)
+end
+
+function meta:__mul(other)
+	if type(other) ~= "table" then
+		return vec_mod(self.x * other, self.y * other)
+	end
+	return vec_mod(self.x * other.x, self.y * other.y)
+end
+
+function meta:__div(other)
+	if type(other) ~= "table" then
+		return vec_mod(self.x / other, self.y / other)
+	end
+	return vec_mod(self.x / other.x, self.y / other.y)
+end
+
+function meta:__tostring()
+	return "vec(" .. tostring(self.x) .. ", " .. tostring(self.y) .. ")"
+end
+
+function vec_mod(x, y)
+	assert(x)
+	assert(y)
+
+	local v = setmetatable({ x = x, y = y }, meta)
 
 	return v
 end
