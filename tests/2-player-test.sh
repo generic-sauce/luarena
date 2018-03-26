ccca4c2773b9b974d45532060a53a1c7dbd327d9
memoryleak47 <memoryleak47@protonmail.ch>
2018-03-26 02:02:58 +0200

    tests/*-player-test.sh now accept characters as args

--- tests/2-player-test.sh
+++ tests/2-player-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-./run.sh server u1 &
+[[ -z $1 ]] && c1=archer || c1=$1
+[[ -z $2 ]] && c2=archer || c2=$2
+
+./run.sh server $c1 &
 sleep 0.5 # this is required, so that the server is always the first!
-./run.sh client u1 "127.0.0.1" &
+./run.sh client $c2 "127.0.0.1" &
 
 read
 
--- tests/3-player-test.sh
+++ tests/3-player-test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-./run.sh server u1 &
+[[ -z $1 ]] && c1=archer || c1=$1
+[[ -z $2 ]] && c2=archer || c2=$2
+[[ -z $3 ]] && c3=archer || c3=$3
+
+./run.sh server $c1 &
 sleep 0.5 # this is required, so that the server is always the first!
-./run.sh client u1 "127.0.0.1" &
+./run.sh client $c2 "127.0.0.1" &
 sleep 1
-./run.sh client u1 "127.0.0.1" &
+./run.sh client $c3 "127.0.0.1" &
 
 read
 
